dadd2094b080102a71902ce0318fff0907fbb240
Miguel <miguel.barreiro@miniclip.com>
2013-03-20 10:56:27 +0000

    tried to be more efficient

--- src/emysql_tcp.erl
+++ src/emysql_tcp.erl
@@ -373,36 +373,22 @@ recv_row_data(Sock, FieldList, _SeqNum, Tid, Key, Fun = {Function, User_data}) -
 	case Res of
 		#packet{seq_num = SeqNum1, data = <<?RESP_EOF, _WarningCount:16/little, ServerStatus:16/little>>} ->
 			%-% io:format("- eof: ~p~n", [emysql_conn:hstate(ServerStatus)]),
-			if
-				is_function(Function)  ->
-					erlang:apply(Function , [{FieldList, no_more_rows , User_data }] ),
-					{SeqNum1, ?ETS_SELECT(Tid), ServerStatus};
-				true->
-					{SeqNum1, ?ETS_SELECT(Tid), ServerStatus}
-			end;
+			erlang:apply(Function , [{FieldList, no_more_rows , User_data }] ),
+			{SeqNum1, ?ETS_SELECT(Tid), ServerStatus};
 
 		#packet{seq_num = SeqNum1, data = <<?RESP_EOF, _/binary>>} ->
 			%-% io:format("- eof.~n", []),
-			if
-				is_function(Function)  ->
-					erlang:apply(Function , [{FieldList, no_more_rows , User_data }] )
-			end,
+			erlang:apply(Function , [{FieldList, no_more_rows , User_data }] ),
 			{SeqNum1, ?ETS_SELECT(Tid), ?SERVER_NO_STATUS};
 
 		#packet{seq_num = SeqNum1, data = RowData} ->
 			%io:format("Seq: ~p raw: ~p~n", [SeqNum1, RowData]),
 			Row = decode_row_data(RowData, FieldList, []),
-			if
-				is_function(Function)  ->
-					case erlang:apply(Function , [{FieldList, Row , User_data }] ) of 
-						{ continue, New_user_data} ->
-					 		recv_row_data(Sock, FieldList, SeqNum1, Tid, Key+1, {Function, New_user_data });
-						stop ->
-					 		{SeqNum1, ?ETS_SELECT(Tid), ?SERVER_NO_STATUS}
-					end;
-				true ->
-					ets:insert(Tid, {Key, Row}),
-					recv_row_data(Sock, FieldList, SeqNum1, Tid, Key+1, Fun)
+			case erlang:apply(Function , [{FieldList, Row , User_data }] ) of 
+				{ continue, New_user_data} ->
+			 		recv_row_data(Sock, FieldList, SeqNum1, Tid, Key+1, {Function, New_user_data });
+				stop ->
+			 		{SeqNum1, ?ETS_SELECT(Tid), ?SERVER_NO_STATUS}
 			end
 	end.
 
